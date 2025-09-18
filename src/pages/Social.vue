a44743425a57b3c1da1caa3b02e5a87faa837936
mikuwithbeer <git@miku.beer>
2025-09-18 23:40:54 +0300

    add social page

---
+++ src/pages/Social.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import Card from "@/components/Card.vue";
+</script>
+
+<template>
+  <div flex="~ col" gap-10>
+    <Card link="mailto:hello@miku.beer"
+          title="Mail"/>
+    <Card link="https://github.com/mikuwithbeer"
+          title="GitHub"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
