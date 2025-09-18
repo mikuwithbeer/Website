27e41a6c5787915a05753243168d083b3a0ced83
mikuwithbeer <git@miku.beer>
2025-09-18 23:40:00 +0300

    add card component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 </script>
 
 <template>
-  <p w="full" h="full"
-     text="dark-100"
-     flex="~ col" items="center" justify="center">
+  <p flex="~ col" h="full"
+     items="center"
+     justify="center" text="dark-100" w="full">
     miku(dot)beer
-    [always a wip]
+    [ミクとビール]
   </p>
 </template>
 
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,21 +1,28 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 </script>
 
 <template>
-  <div grid="~ cols-5 rows-5 gap-0"
-       w="30rem" h="30rem">
-    <header col="start-1 end-2" row="start-1 end-6"
-            b="~ solid dark-400 r-0">
+  <div grid="~ cols-6 rows-6 gap-0"
+       h="30rem" w="30rem">
+    <header b="~ solid dark-400 r-0" col="start-1 end-2"
+            row="start-1 end-7">
       <slot name="header"></slot>
     </header>
-    <main col="start-2 end-6" row="start-1 end-5"
-          b="~ solid dark-400 b-0">
+    <main b="~ solid dark-400 b-0" col="start-2 end-7"
+          overflow="auto"
+          p="10"
+          row="start-1 end-6">
       <slot name="main"></slot>
     </main>
-    <footer col="start-2 end-6" row="start-5 end-6"
-            b="~ solid dark-400">
+    <footer b="~ solid dark-400" col="start-2 end-6"
+            row="start-6 end-7">
       <slot name="footer"></slot>
     </footer>
+    <div b="~ solid dark-400 l-0" col="start-6 end-7"
+         flex="~ col" items-center
+         justify-center row="start-6 end-7" text-pink>
+      テト
+    </div>
   </div>
 </template>
 
