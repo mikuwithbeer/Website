62e5b99824b75f9cb556eddfc625a0c378f7214c
mikuwithbeer <git@miku.beer>
2026-06-13 02:39:46 +0300

    bump version

--- src/pages/Education.vue
+++ src/pages/Education.vue
@@ -4,10 +4,19 @@ import Link from "@/components/Link.vue";
 
 <template>
   <div>
-    <Link content="bogazici university" keep link="https://bogazici.edu.tr/en" />
-    <p>department of management</p>
-    <p>istanbul, türkiye</p>
-    <p>2025 -> 2030</p>
+    <div>
+      <Link content="bogazici university" keep link="https://yadyok.bogazici.edu.tr/en" />
+      <p>school of foreign languages</p>
+      <p>istanbul, türkiye</p>
+      <p>2025 -> 2026</p>
+    </div>
+    <br />
+    <div>
+      <Link content="bogazici university" keep link="https://bogazici.edu.tr/en" />
+      <p>department of management</p>
+      <p>istanbul, türkiye</p>
+      <p>2026 -> 2030</p>
+    </div>
   </div>
 </template>
 
